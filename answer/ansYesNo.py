50b16f6b53c4d07a61d67ca208bbaf3d3d51d976
yutongl1 <yutongl1@andrew.cmu.edu>
2018-04-17 19:47:13 -0400

    Update YesNo

--- answer/ansYesNo.py
+++ answer/ansYesNo.py
@@ -1,9 +1,67 @@
+# Preprocessor for the question generation 
+# The preprocessor extracts sentences from .txt wikipedia articles, remove any noises, and tokenize sentences into words. 
+import nltk
+from nltk import word_tokenize, sent_tokenize,pos_tag
+import os
+import sys
+from nltk.parse import stanford
+from nltk.parse.stanford import StanfordParser
+from nltk.parse.stanford import StanfordDependencyParser
+from nltk.parse.stanford import StanfordNeuralDependencyParser
+from nltk.tag.stanford import StanfordPOSTagger, StanfordNERTagger
+from nltk.tokenize.stanford import StanfordTokenizer
+from nltk.stem import WordNetLemmatizer
+import string
+import csv
+from difflib import SequenceMatcher
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import linear_kernel
+
+lemmatizer = WordNetLemmatizer()
+stop_words = set([u'all', u'just', u'being', u'over', u'both', u'through', u'yourselves', u'its', u'before', u'o', u'hadn', u'herself', u'll', u'had', u'should', u'to', u'only', u'won', u'under', u'ours', u'has', u'do', u'them', u'his', u'very', u'they', u'during', u'now', u'him', u'nor', u'd', u'did', u'didn', u'this', u'she', u'each', u'further', u'where', u'few', u'because', u'doing', u'some', u'hasn', u'are', u'our', u'ourselves', u'out', u'what', u'for', u'while', u're', u'does', u'above', u'between', u'mustn', u't', u'be', u'we', u'who', u'were', u'here', u'shouldn', u'hers', u'by', u'on', u'about', u'couldn', u'of', u'against', u's', u'isn', u'or', u'own', u'into', u'yourself', u'down', u'mightn', u'wasn', u'your', u'from', u'her', u'their', u'aren', u'there', u'been', u'whom', u'too', u'wouldn', u'themselves', u'weren', u'was', u'until', u'more', u'himself', u'that', u'but', u'don', u'with', u'than', u'those', u'he', u'me', u'myself', u'ma', u'these', u'up', u'will', u'below', u'ain', u'can', u'theirs', u'my', u'and', u've', u'then', u'is', u'am', u'it', u'doesn', u'an', u'as', u'itself', u'at', u'have', u'in', u'any', u'if', u'again', u'no', u'when', u'same', u'how', u'other', u'which', u'you', u'shan', u'needn', u'haven', u'after', u'most', u'such', u'why', u'a', u'off', u'i', u'm', u'yours', u'so', u'y', u'the', u'having', u'once'])
+
+
+# Use stanford pos for lemmatization here 
+# The Pos Tagger in NLTK will recognize words like "sits", "walks" as "NNS" instead of verb. 
+stanford_pos = '../stanford/stanford-postagger-full-2015-04-20/'
+stanford_pos_model = stanford_pos + 'models/english-left3words-distsim.tagger'
+stanford_pos_jar = stanford_pos + 'stanford-postagger.jar'
+st_pos = StanfordPOSTagger(model_filename=stanford_pos_model, path_to_jar=stanford_pos_jar)
+
+
+# # NER Tagging:
+stanford_ner = '../stanford/stanford-ner-2015-04-20/'
+stanford_ner_model = stanford_ner + 'classifiers/english.muc.7class.distsim.crf.ser.gz'
+stanford_ner_jar = stanford_ner + 'stanford-ner.jar'
+ner = StanfordNERTagger(model_filename=stanford_ner_model, path_to_jar=stanford_ner_jar)
+
+
+# Set up the stanford PCFG parser
+stanford_parser_dir = '../stanford/stanford-parser-full-2015-04-20/'
+eng_model_path = stanford_parser_dir  + "englishPCFG.ser.gz"
+my_path_to_models_jar = stanford_parser_dir  + "stanford-parser-3.5.2-models.jar"
+my_path_to_jar = stanford_parser_dir  + "stanford-parser.jar"
+parser = StanfordParser(model_path=eng_model_path, path_to_models_jar=my_path_to_models_jar, path_to_jar=my_path_to_jar)
+
+stopWords = stopwords.words('english')
+
+negation_words = ['not', 'few']
+
+
+# Intersection between two lists
+def intersection(lst1, lst2):
+		lst3 = [value for value in lst1 if value in lst2]
+		return lst3
+
+
 def ansYesNo(question, max_similar_sent, max_similarity):
 	# Yes/No question: answer should contain only yes or no.  
 
-	# print "Question:", ' '.join(question)
-	# print "Selected:", ' '.join(max_similar_sent)
-
+	question_pos = st_pos.tag(question)
+	max_similar_pos = st_pos.tag(max_similar_sent)
 
 	question_parse = parser.parse(question)
 	for parse in question_parse:
@@ -11,23 +69,57 @@ def ansYesNo(question, max_similar_sent, max_similarity):
 		sub = parse[0][1].leaves()
 		try:
 			obj = parse[0][2].leaves()
+			obj = [w for w in question if w not in verb and w not in sub]
 		except: 
 			obj = sub 
 
-	obj = [lemmatizer.lemmatize(i,j[0].lower()) if j[0].lower() in ['a','n','v'] else lemmatizer.lemmatize(i) for i,j in st_pos.tag(obj)]
-	max_similar_sent = [lemmatizer.lemmatize(i,j[0].lower()) if j[0].lower() in ['a','n','v'] else lemmatizer.lemmatize(i) for i,j in st_pos.tag(max_similar_sent)]
+		
+	# Remove stopwords and punctuations
+	obj = [w for w in obj if not w in stop_words and not w in string.punctuation]
+	selected = [w for w in max_similar_sent if not w in stop_words]
 
-	intersect = intersection(obj,max_similar_sent)
+
+	# Lemmatize
+	obj = [lemmatizer.lemmatize(w,t[0].lower()) if t[0].lower() in ['a','n','v'] else lemmatizer.lemmatize(w) for w,t in question_pos if w in obj]
+	selected = [lemmatizer.lemmatize(w,t[0].lower()) if t[0].lower() in ['a','n','v'] else lemmatizer.lemmatize(w) for w,t in max_similar_pos if w in selected]
+	# Calculate how many overlapped words is in the question. 
+	intersect = intersection(obj,selected)
 	recall =  float(len(intersect)) / len(obj)
+
+
+
+	print "--------"
+	print "Intersect:", intersect
+	print "In question:", obj
+	print "In Sentence", selected
+	print "Recall:", recall
+	print "--------"
 	# print "Score Cosine:", max_similarity
 	# print "Score Recall:", recall
-	if recall  >= 0.6:
-		# Problem: they are not farmed to any extent , but wild kangaroos are shot for meat
-		# Asking about the first half and asking about the second half. 
-		if "not" not in intersect and "not" in question or "not" in max_similar_sent:
-			answer = "No"
-		else:
-			answer = "Yes"
+	if recall  > 0.6:
+		answer = "Yes"
+		for neg_words in negation_words:
+			if neg_words not in intersect and neg_words in question:
+				try:
+					after_not, tag = question_pos[question.index(neg_words) + 1]
+				except:
+					continue
+
+				if tag[0].lower() in ['a','n','v']:
+					after_not = lemmatizer.lemmatize(after_not,tag[0].lower())
+				if after_not in intersect:
+					answer = "No"
+
+			if neg_words not in intersect and neg_words in max_similar_sent:
+				try:
+					after_not, tag = max_similar_pos[max_similar_sent.index(neg_words) + 1]
+				except:
+					continue
+
+				if tag[0].lower() in ['a','n','v']:
+					after_not = lemmatizer.lemmatize(after_not,tag[0].lower())
+				if after_not in intersect:
+					answer = "No"			
 	else:
 		answer = "No"
 
