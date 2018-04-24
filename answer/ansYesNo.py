cf72ebffeeb56d1bfb9a09e333ecab08ed6bb9a3
AmberYZ <yuyan.zhang.950310@gmail.com>
2018-04-23 22:14:07 -0400

    final version

--- answer/ansYesNo.py
+++ answer/ansYesNo.py
@@ -19,6 +19,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 from nltk.corpus import stopwords
 from sklearn.metrics.pairwise import linear_kernel
+import re
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set([u'all', u'just', u'being', u'over', u'both', u'through', u'yourselves', u'its', u'before', u'o', u'hadn', u'herself', u'll', u'had', u'should', u'to', u'only', u'won', u'under', u'ours', u'has', u'do', u'them', u'his', u'very', u'they', u'during', u'now', u'him', u'nor', u'd', u'did', u'didn', u'this', u'she', u'each', u'further', u'where', u'few', u'because', u'doing', u'some', u'hasn', u'are', u'our', u'ourselves', u'out', u'what', u'for', u'while', u're', u'does', u'above', u'between', u'mustn', u't', u'be', u'we', u'who', u'were', u'here', u'shouldn', u'hers', u'by', u'on', u'about', u'couldn', u'of', u'against', u's', u'isn', u'or', u'own', u'into', u'yourself', u'down', u'mightn', u'wasn', u'your', u'from', u'her', u'their', u'aren', u'there', u'been', u'whom', u'too', u'wouldn', u'themselves', u'weren', u'was', u'until', u'more', u'himself', u'that', u'but', u'don', u'with', u'than', u'those', u'he', u'me', u'myself', u'ma', u'these', u'up', u'will', u'below', u'ain', u'can', u'theirs', u'my', u'and', u've', u'then', u'is', u'am', u'it', u'doesn', u'an', u'as', u'itself', u'at', u'have', u'in', u'any', u'if', u'again', u'no', u'when', u'same', u'how', u'other', u'which', u'you', u'shan', u'needn', u'haven', u'after', u'most', u'such', u'why', u'a', u'off', u'i', u'm', u'yours', u'so', u'y', u'the', u'having', u'once'])
@@ -94,7 +95,9 @@ def ansYesNo(question, max_similar_sent, max_similarity, title):
 	# Lemmatize
 	tag_dict = {"j":"a", "n":"n", "v": "v"}
 	obj = [lemmatizer.lemmatize(w,tag_dict[t[0].lower()]) if t[0].lower() in ['a','n','v'] else lemmatizer.lemmatize(w) for w,t in question_pos if w in obj]
+	obj = [w for w in obj if re.search('[a-zA-Z1-9]', w) and w != "'s"]
 	selected = [lemmatizer.lemmatize(w,tag_dict[t[0].lower()]) if t[0].lower() in ['a','n','v'] else lemmatizer.lemmatize(w) for w,t in max_similar_pos if w in selected]
+	selected = [w for w in selected if re.search('[a-zA-Z1-9]', w) and w != "'s"]
 	# Calculate how many overlapped words is in the question. 
 	intersect = intersection(obj,selected)
 
